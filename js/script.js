49421a9b96dcfee50589b4e4d609fb46ef26b8db
Guilherme147828 <guilherme.s.silva36@aluno.senai.br>
2024-10-31 11:04:20 -0300

    tela-login-aula

--- js/script.js
+++ js/script.js
@@ -1,53 +1,72 @@
 const form = document.getElementById('form');
+const name_input = document.getElementById('name-input');
 const email_input = document.getElementById('email-input');
 const password_input = document.getElementById('password-input');
+const repeat_password_input = document.getElementById('repeat-password-input');
 const error_message = document.getElementById('error-message');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let errors = [];
 
-    errors = validateLoginForm(email_input.value, password_input.value);
+    const FileName = e.target.baseURI.split("/").pop();
 
-    //verificar login
-    if (errors.length === 0) {
-        const isValidLogin = verifyLogin(
+    if (FileName === "register.html") {
+        errors = validateRegisterForm(
+            name_input.value,
+            email_input.value,
+            password_input.value,
+            repeat_password_input.value
+        );
+    } else {
+        errors = validateLoginForm(
             email_input.value,
             password_input.value
         );
-        if (!isValidLogin) {
-            errors.push('Email ou senha incorretos');
+
+        //Verificar Login
+        if (errors.length === 0) {
+            const isValidLogin = verifyLogin(
+                email_input.value,
+                password_input.value
+            );
+            if (!isValidLogin) {
+                errors.push('Email ou senha incorretos');
+            }
         }
     }
+
 
     if (errors.length > 0) {
         error_message.innerText = errors.join(". ");
         return;
     }
 
-    window.location.href = "home.html";
+    window.location.href = 'home.html';
 });
 
 function validateLoginForm(email, password) {
     let errors = [];
 
     if (email === "" || email == null) {
-        errors.push('Email é obrigatorio');
+        errors.push('Email é obrigatório');
         email_input.parentElement.classList.add("incorrect");
     }
 
-    if (password === "" || password == null) {
-        errors.push('Email é obrigatorio');
+    if (password === "" || email == null) {
+        errors.push('Senha é obrigatório');
         password_input.parentElement.classList.add("incorrect");
     }
 
     return errors;
 }
 
-
 const allInputs = [
+    name_input,
     email_input,
-    password_input,
+    password_input,[
+    repeat_password_input
+    ]
 ].filter((input) => input != null);
 
 allInputs.forEach((input) => {
@@ -62,7 +81,7 @@ allInputs.forEach((input) => {
 // Verificar login
 function verifyLogin(email, password) {
     const request = new XMLHttpRequest();
-    request.open("GET", "./data/users.json", false);
+    request.open("GET", "./data/user.json", false);
     request.send(null);
 
     localStorage.removeItem("userEmail");
@@ -82,3 +101,39 @@ function verifyLogin(email, password) {
         return false;
     }
 }
+function validateRegisterForm(name, email, password, repeatPassword) {
+    let errors = [];
+  
+    if (name === "" || name == null) {
+      errors.push("Nome é obrigatório");
+      name_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (email === "" || email == null) {
+      errors.push("Email é obrigatório");
+      email_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (password === "" || password == null) {
+      errors.push("Senha é obrigatória");
+      password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password.length < 8) {
+        errors.push("Senha deve ter no mínimo 8 caracteres");
+        password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    if (repeatPassword === "" || repeatPassword == null) {
+      errors.push("Repita a senha");
+      repeat_password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password !== repeatPassword) {
+        errors.push("Senhas não conferem");
+        password_input.parentElement.classList.add("incorrect");
+        repeat_password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    return errors;
+  }
